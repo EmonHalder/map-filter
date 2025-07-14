bc446fa1537764a104d4bdb953011dc4d0b9e3b6
Ariful4a <afkgamingn01@gmail.com>
2025-07-14 11:35:03 +0600

    ADDED MORE NUMBER

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function Counter() {
   const [count, setCount] = useState(0)
 
-  const numbers = [1, 2, 3, 4, 5, 6]
+  const numbers = [1, 2, 3, 4, 5, 6, 8]
   const evenNumbers = numbers.filter(num => num % 2 === 0)
   const doubledEvens = evenNumbers.map(num => num * 2)
 
